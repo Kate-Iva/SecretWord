cc8266ee7843f88e345ab55afeef9b39d9834b9e
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-20 14:47:40 +0100

    Add words.js and update App.jsx

---
+++ src/data/wordsPt.js
@@ -0,0 +1,8 @@
+export const words = {
+    rosto: ['rosto', 'boca', 'olho', 'nariz', 'bochecha'],
+    frutas: ['banana', 'maçã', 'laranja', 'uva', 'morango'],
+    corpo: ['corpo', 'braço', 'perna', 'mão', 'cabeça'],
+    computador: ['computador', 'teclado', 'mouse', 'monitor', 'impressora'],
+    programacao: ['programacao', 'codigo', 'algoritmo', 'função', 'variável'],
+    comida: ['comida', 'arroz', 'feijão', 'carne', 'peixe']
+  };
