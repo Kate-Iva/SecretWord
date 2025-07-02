d9237ff4921d8a5707c81fa9cb9a031ac3c5a63a
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-07-02 23:34:50 +0100

    Estruturando Componente do Jogo

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,35 @@
 import styles from "./Game.module.css"
 
-const Game = () => {
-    return <div className={styles.game} >Game</div>;
+const Game = ({ verifyLetter }) => {
+    return (
+    <div className={styles.game} >
+    <p className={styles.points}>
+     <span>Pontuação: 000 </span>
+    </p>
+    <h1>Advinhe a palavra:</h1>
+    <h3 className={styles.tip}>
+        Dica soobre a palavra: <span>Dica...</span>
+    </h3>
+    <div className={styles.wordContainer}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blankSquare}></span>
+    </div>
+    <div className={styles.letterContainer}>
+         <p>Tente advinhar uma letra da palavra:</p>
+         <form>
+            <input type="text" name="letter" maxLength="1" required  />
+            <button>Jogar</button>
+         </form>
+    </div>
+
+    <div className={styles.wrongLettersContainer}>
+        <p>Letras já utilizadas: </p>
+        <span>a, </span>
+        <span>b, </span>
+    </div>
+    </div>
+    
+);
 };
 
 export default Game;
